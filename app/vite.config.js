9e6664aab28c3daf3eedd0fff66259f354a6a7a8
furuyamik2 <furuya002.3020junjun@gmail.com>
2025-04-20 12:20:51 +0900

    Revert "login"
    This reverts commit 9486092e0bd31fa9944076e7a9df71516490fe91.

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,19 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    // ここから追加
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: 'localhost',
-        },
-    },
-    // ここまで追加
 });
